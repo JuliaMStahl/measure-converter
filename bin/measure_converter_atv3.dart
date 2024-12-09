14b2b1057ff471cf93c54fc389270f18c8220b10
Julia Stahl <juliastahl@alunos.utfpr.edu.br>
2024-12-09 20:01:08 -0300

    Commit inicial

---
+++ bin/measure_converter_atv3.dart
@@ -0,0 +1,5 @@
+import 'package:measure_converter_atv3/measure_converter_atv3.dart' as measure_converter_atv3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${measure_converter_atv3.calculate()}!');
+}
---
+++ test/measure_converter_atv3_test.dart
@@ -0,0 +1,8 @@
+import 'package:measure_converter_atv3/measure_converter_atv3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
